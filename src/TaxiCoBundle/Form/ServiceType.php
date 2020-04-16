3e5430c5dfef0b2c3ea503c068b9d5a215e1e979
Walid Ben Othman <60236281+walidbo@users.noreply.github.com>
2020-04-16 20:44:27 +0100

    maintenance

---
+++ src/TaxiCoBundle/Form/ServiceType.php
@@ -0,0 +1,40 @@
+<?php
+
+namespace TaxiCoBundle\Form;
+
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\SubmitType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+use Gregwar\CaptchaBundle\Type\CaptchaType;
+
+class ServiceType extends AbstractType
+{
+    /**
+     * {@inheritdoc}
+     */
+    public function buildForm(FormBuilderInterface $builder, array $options)
+    {
+        $builder->add('name');
+        $builder->add('captcha', CaptchaType::class);
+//        $builder->add('Add',SubmitType::class);
+    }/**
+     * {@inheritdoc}
+     */
+    public function configureOptions(OptionsResolver $resolver)
+    {
+        $resolver->setDefaults(array(
+            'data_class' => 'TaxiCoBundle\Entity\Service'
+        ));
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function getBlockPrefix()
+    {
+        return 'taxicobundle_service';
+    }
+
+
+}
