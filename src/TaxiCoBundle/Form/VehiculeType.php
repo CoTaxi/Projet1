56275053583a5ce3858afb1caa63397ee72e8596
achrefnasri <achref.nasri@esprit.tn>
2020-05-29 19:26:06 +0100

    vec

--- src/TaxiCoBundle/Form/VehiculeType.php
+++ src/TaxiCoBundle/Form/VehiculeType.php
@@ -3,10 +3,12 @@
 namespace TaxiCoBundle\Form;
 
 use Gregwar\CaptchaBundle\Type\CaptchaType;
+use Symfony\Component\DomCrawler\Field\TextareaFormField;
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\Extension\Core\Type\ChoiceType;
 use Symfony\Component\Form\Extension\Core\Type\FileType;
 use Symfony\Component\Form\Extension\Core\Type\SubmitType;
+use Symfony\Component\Form\Extension\Core\Type\TextareaType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
 
@@ -18,27 +20,57 @@ class VehiculeType extends AbstractType
     public function buildForm(FormBuilderInterface $builder, array $options)
     {
         $builder->add('matricule',FileType::class, array('data_class'=>null, 'required'=>false))
-            ->add('marque',ChoiceType::class,array(
-                'choices'=>array(
-                    'Alfa Romeo'=>"Alfa Romeo",
-                    'Audi'=>"Audi",
-                    'Citroen'=>"Citroen",
-                    'Ford'=>"Ford",
-                    'Peugeot'=>"Peugeot",)))
+            ->add('marque',ChoiceType::class,array('choices'=> array(
+
+                'Alfa Romeo'=> 'Alfa Romeo' ,
+                'Audi'=> 'Audi' ,
+                'Ford'=> 'Ford' ,
+                'Peugeot'=> 'Peugeot' ,
+            )))
             ->add('modele')
             ->add('cartegrise',FileType::class, array('data_class'=>null, 'required'=>false))
-            ->add('couleur')
-            ->add('type',ChoiceType::class,array(
-                'choices'=>array(
-                    'Taxi'=>"taxi",
-                    'Covoiturage'=>"covoiturage",)))
-            ->add('position')
+            ->add('couleur',ChoiceType::class,array('choices'=> array(
+
+                'Blanc'=> 'Blanc' ,
+                'Bleu'=> 'Bleu' ,
+                'Rouge'=> 'Rouge' ,
+                'Vert'=> 'Vert' ,
+                'Noir'=> 'Noir' ,
+                'Jaune'=> 'Jaune' ,
+            )))
+            ->add('type',ChoiceType::class,array('choices'=> array(
+
+                'Covoiturage'=> 'Covoiturage' ,
+                'Taxi'=> 'Taxi' ,
+            )))
+            ->add('position',ChoiceType::class,array('choices'=> array(
+
+                'Aryanah'=> 'Aryanah' ,
+                'Bardo'=> 'Bardo' ,
+                'Centre ville'=> 'Centre ville' ,
+            )))
             ->add('destination')
-            ->add('places')
-            ->add('zone')
-            ->add('acceptC')
+            ->add('places',ChoiceType::class,array('choices'=> array(
+
+                '1'=> '1' ,
+                '2'=> '2' ,
+                '3'=> '3' ,
+                '4'=> '4' ,
+            )))
+            ->add('zone',ChoiceType::class,array('choices'=> array(
+
+                'Nord'=> 'Nord' ,
+                'Sud'=> 'Sud' ,
+                'East'=> 'East' ,
+                'Ouest'=> 'Ouest' ,
+            )))
+            ->add('acceptC',ChoiceType::class,array('choices'=> array(
+
+                'Oui'=> 'Oui' ,
+                'Non'=> 'Non' ,
+            )))
             ->add('captcha', CaptchaType::class)
-            ->add('Ajouter Vehicule',SubmitType::class);
+;
     }/**
      * {@inheritdoc}
      */
