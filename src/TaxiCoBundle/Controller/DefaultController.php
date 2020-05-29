9f4b33e246e88b38d5e3f25246f3c05fbacf4537
meriammhadhebi <49176816+meriammhadhebi@users.noreply.github.com>
2020-05-29 19:24:50 +0100

    commit

--- src/TaxiCoBundle/Controller/DefaultController.php
+++ src/TaxiCoBundle/Controller/DefaultController.php
@@ -3,11 +3,16 @@
 namespace TaxiCoBundle\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\HttpFoundation\Request;
 
 class DefaultController extends Controller
 {
     public function indexAction()
     {
         return $this->render('default/acceuil.html.twig');
+    }
+    public function ourservicesAction()
+    {
+        return $this->render('default/ourservices.html.twig');
     }
 }
